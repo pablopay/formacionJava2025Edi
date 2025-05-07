b0b8a9b3d2dd73955fefc0acf255a3357215bc32
pablopay <163046661+pablopay@users.noreply.github.com>
2025-05-07 16:47:02 +0200

    todo del qr

---
+++ src/main/java/com/edisa/formacion/mayo2025/Parte2.java
@@ -0,0 +1,38 @@
+package com.edisa.formacion.mayo2025;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Parte2 {
+    public static void main(String[] args) throws WriterException, IOException {
+        if (args.length == 0) {
+            System.out.println("Por favor, proporciona un texto para el código QR.");
+            return;
+        }
+
+        String data = args[0];
+        String path = args[1];
+        String format = args[2];
+
+        BarcodeFormat barcodeFormat;
+
+        try {
+            barcodeFormat = BarcodeFormat.valueOf(format);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Formato no válido. Usa, por ejemplo: QR_CODE, CODE_128, etc.");
+            return;
+        }
+
+        BitMatrix matrix = new MultiFormatWriter().encode(data, barcodeFormat, 500, 500);
+        MatrixToImageWriter.writeToPath(matrix, "jpg", Paths.get(path));
+
+
+        System.out.println("QR code successfully created!");
+    }
+}
