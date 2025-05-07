f83ef7dedf8cb1437d1f4d7ccc915593f6518a10
Ismael Álvarez Iglesias <ismael.alvarez@edisa.com>
2025-05-07 13:14:15 +0200

    parte 1 del qr

--- src/main/java/com/edisa/formacion/mayo2025/Main.java
+++ src/main/java/com/edisa/formacion/mayo2025/Main.java
@@ -1,8 +1,31 @@
 package com.edisa.formacion.mayo2025;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Numero de argumentos"+String.valueOf(args.length));
-        System.out.println("Hello world! ");
+    public static void main(String[] args) throws WriterException, IOException {
+        if (args.length == 0) {
+            System.out.println("Por favor, proporciona un texto para el código QR.");
+            return;
+        }
+
+        String data = args[0];
+
+        String path = args[1];
+
+
+        BitMatrix matrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, 500, 500);
+
+        MatrixToImageWriter.writeToPath(matrix, "jpg", Paths.get(path));
+
+        System.out.println("QR code successfully created!");
     }
 }
+//"C:\\Users\\pablo.locay\\Desktop\\prueba.jpg";
