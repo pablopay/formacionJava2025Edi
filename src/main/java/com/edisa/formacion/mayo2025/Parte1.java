2f8d23daf03a2a3aaaa62ba6d4e6d90ac984852e
pablopay <163046661+pablopay@users.noreply.github.com>
2025-05-07 17:18:58 +0200

    terminado

--- src/main/java/com/edisa/formacion/mayo2025/Parte1.java
+++ src/main/java/com/edisa/formacion/mayo2025/Parte1.java
@@ -11,20 +11,44 @@ import java.nio.file.Paths;
 //"Hola mundo" "C:\\Users\\pablo.locay\\Desktop\\qr.jpg"
 
 public class Parte1 {
-    public static void main(String[] args) throws WriterException, IOException {
-        if (args.length == 0) {
-            System.out.println("Por favor, proporciona un texto para el código QR.");
-            return;
+    public static void main(String[] args) {
+
+
+        try{
+
+            if (args.length == 0) {
+                throw new IllegalArgumentException("Por favor, proporciona un texto para el código QR y su ruta.");
+
+            }
+
+
+
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido una excepcion:\n"+e.getMessage());
         }
+
 
         String data = args[0];
 
         String path = args[1];
 
 
-        BitMatrix matrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, 500, 500);
+        BitMatrix matrix = null;
+        try {
+            matrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, 500, 500);
+        } catch (WriterException e) {
+            System.out.println("Error al construir la matriz de bits :\n"+e.getMessage());
+        }
 
-        MatrixToImageWriter.writeToPath(matrix, "jpg", Paths.get(path));
+        if(matrix!=null){
+            try {
+                MatrixToImageWriter.writeToPath(matrix, "jpg", Paths.get(path));
+            } catch (IOException e) {
+                System.out.println("El Qr no se escribio correctamente :\n"+e.getMessage());
+            }
+        }
+
+
 
         System.out.println("QR code successfully created!");
     }
